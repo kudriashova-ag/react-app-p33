e46039c881c3e284df25d34f3dd406c16be465cc
Kudriashova Anastasiia <kudriashova.ag@gmail.com>
2025-11-25 19:26:04 +0200

    edit todolist

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TodoItem = ({ item, removeTask }) => {
+const TodoItem = ({ item, removeTask, toggleDone, editTitle }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [title, setTitle] = useState(item.title);
+
+  const normalTemplate = (
+    <span
+      onClick={() => setIsEdit(true)}
+      style={{ textDecoration: item.done ? "line-through" : "none" }}
+    >
+      {item.title}
+    </span>
+  );
+
+  const editTemplate = (
+    <input
+      type="text"
+      autoFocus={true}
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      onBlur={() => {
+        editTitle(item.id, title);
+        setIsEdit(false);
+      }}
+    />
+  );
+
+
   return (
     <div className="todo-item">
-      <input type="checkbox" defaultChecked={item.done} />
-      <span style={{textDecoration: item.done ? 'line-through' : 'none'}}>{item.title}</span>
+      <input
+        type="checkbox"
+        defaultChecked={item.done}
+        onClick={() => toggleDone(item.id)}
+      />
+
+      { isEdit ? editTemplate : normalTemplate }
+
       <button className="remove" onClick={() => removeTask(item.id)}>
         Delete
       </button>
--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -17,6 +17,27 @@ const TodoList = () => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  const toggleDone = (id) => { 
+    const newTasks = tasks.map(task => { 
+      if (task.id === id) { 
+        return {...task, done: !task.done}
+      }
+      return task;
+    }) 
+    setTasks(newTasks);
+  }
+
+  const editTitle = (id, title) => { 
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, title: title };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+
   return (
     <div className="todo">
       <h1>Todo List</h1>
@@ -25,12 +46,20 @@ const TodoList = () => {
         <TodoFilter />
         <div className="todo-list">
           {tasks.map((task) => (
-            <TodoItem key={task.id} item={task} removeTask={removeTask} />
+            <TodoItem
+              key={task.id}
+              item={task}
+              removeTask={removeTask}
+              toggleDone={toggleDone}
+              editTitle={editTitle}
+            />
           ))}
         </div>
       </div>
 
-      <div>Done: { tasks.filter(task=>task.done).length } from { tasks.length }</div>
+      <div>
+        Done: {tasks.filter((task) => task.done).length} from {tasks.length}
+      </div>
     </div>
   );
 };
