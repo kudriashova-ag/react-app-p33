e31e05540e2746c09f53347f926939bc3042ca10
Kudriashova Anastasiia <kudriashova.ag@gmail.com>
2025-12-15 19:25:47 +0200

    inifiniteScroll

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -2,7 +2,10 @@ import React, { useEffect, useRef, useState } from "react";
 import { Link, useFetcher, useLoaderData, useLocation } from "react-router";
 
 const SearchResults = () => {
-  const users = useLoaderData();
+  const initialData = useLoaderData();
+  
+  const [users, setUsers] = useState(initialData.items);
+
   const [page, setPage] = useState(2);
   const fetcher = useFetcher();
 
@@ -13,16 +16,22 @@ const SearchResults = () => {
   const params = new URLSearchParams(location.search);
   const text = params.get("text");
 
-  useEffect(() => { }, [fetcher.state, fetcher.data])
-
-
+  useEffect(() => {
+    if (fetcher.state === "idle" && fetcher.data) {
+      if(fetcher.data.items.length > 0){
+        setUsers([...users, ...fetcher.data.items]);
+        setPage(page + 1);
+      }
+    }
+  }, [fetcher.state, fetcher.data]);
 
   useEffect(() => {
+    if (fetcher.state === "loading") return;
+
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
           fetcher.load(`/search?text=${text}&page=${page}`);
-          setPage(page + 1);
         } else {
           // console.log("not visible");
         }
@@ -34,20 +43,23 @@ const SearchResults = () => {
     );
 
     observer.observe(lastElementRef.current);
-    
-  }, []);
+  }, [page, text]);
 
-  // при прокрутці до останнього юзера - новий запит
-  // http://localhost:3000/search?text=pir&page=2
+
+  useEffect(() => {
+    setUsers(initialData.items);
+    setPage(2)
+  }, [initialData]);
+
 
   return (
     <div>
       <h1>Search Results</h1>
       <div className="users-list">
-        {users.items.map((user, index) => (
+        {users.map((user, index) => (
           <div
             key={user.id}
-            ref={index === users.items.length - 1 ? lastElementRef : null}
+            ref={index === users.length - 1 ? lastElementRef : null}
           >
             <Link to={`/user/${user.login}`}>
               <img src={user.avatar_url} alt="" />
